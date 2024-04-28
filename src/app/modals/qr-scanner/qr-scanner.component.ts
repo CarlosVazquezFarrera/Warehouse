e52d06b6f00ed74c93a7552a2d250e4377039a9d
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2024-04-28 17:09:58 -0500

    Back camera as default

--- src/app/modals/qr-scanner/qr-scanner.component.ts
+++ src/app/modals/qr-scanner/qr-scanner.component.ts
@@ -1,10 +1,12 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ViewChild, inject } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, Component, ViewChild, inject } from '@angular/core';
 import { ModalHeaderComponent } from '@shared/components/modal-header/modal-header.component';
 import { NgxScannerQrcodeModule, LOAD_WASM, NgxScannerQrcodeComponent, ScannerQRCodeResult, ScannerQRCodeDevice, ScannerQRCodeConfig } from 'ngx-scanner-qrcode';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AbstractControl, FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { filter } from 'rxjs';
+import { Platform } from '@angular/cdk/platform';
 
 
 LOAD_WASM().subscribe();
@@ -18,39 +20,26 @@ LOAD_WASM().subscribe();
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QrScannerComponent implements AfterViewInit {
-  // public config: ScannerQRCodeConfig = {
-  //   constraints: {
-  //     video: {
-  //       width: window.innerWidth
-  //     },
-  //   },
-  //   canvasStyles: [
-  //     { /* layer */
-  //       lineWidth: 1,
-  //       fillStyle: '#00950685',
-  //       strokeStyle: '#00950685',
-  //     },
-  //     { /* text */
-  //       font: '17px serif',
-  //       fillStyle: '#ff0000',
-  //       strokeStyle: '#ff0000',
-  //     }
-  //   ],
-  // };
 
   private fb = inject(FormBuilder);
   public fbCamera = this.fb.group({
     camera: ['']
   });
+  public platform = inject(Platform);
 
-  public changeCamera() {
-
-  }
-  ngAfterViewInit(): void {
+  ngAfterViewInit() {
     this.scan.start();
+    this.scan.devices
+      .pipe(
+        filter(devices => devices.length > 0))
+      .subscribe(devices => {
+        if (!this.platform.ANDROID || ! this.platform.IOS) return;
+
+        const device = devices.find(f => (/back|rear|environment/gi.test(f.label)));
+        this.scan.playDevice(device ? device.deviceId : devices[0].deviceId);
+      });
     this.camera.valueChanges.subscribe(_ => this.cameraHasChange());
   }
-
   private cameraHasChange() {
     this.scan.playDevice(this.camera.value!);
   }
