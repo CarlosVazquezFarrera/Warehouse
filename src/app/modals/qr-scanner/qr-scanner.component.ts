08eefff2a716a4051bb9bef66290a116f774b1ea
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2024-04-30 14:10:41 -0500

    ZXingScannerComponent test

--- src/app/modals/qr-scanner/qr-scanner.component.ts
+++ src/app/modals/qr-scanner/qr-scanner.component.ts
@@ -1,6 +1,5 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ViewChild, inject } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ModalHeaderComponent } from '@shared/components/modal-header/modal-header.component';
-import { NgxScannerQrcodeModule, LOAD_WASM, NgxScannerQrcodeComponent, ScannerQRCodeResult, ScannerQRCodeDevice } from 'ngx-scanner-qrcode';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
@@ -16,12 +15,11 @@ import { sleep } from '@shared/helper/sleep';
 import { environment } from '@environments/environment';
 import { QrReaderComponent } from '@shared/components/qr-reader/qr-reader.component';
 
-LOAD_WASM().subscribe();
 
 @Component({
   selector: 'app-qr-scanner',
   standalone: true,
-  imports: [ModalHeaderComponent, NgxScannerQrcodeModule, MatSelectModule, MatDialogModule, ReactiveFormsModule, MatFormFieldModule, MatButtonModule, MatIconModule, QrReaderComponent],
+  imports: [ModalHeaderComponent, MatSelectModule, MatDialogModule, ReactiveFormsModule, MatFormFieldModule, MatButtonModule, MatIconModule, QrReaderComponent],
   templateUrl: './qr-scanner.component.html',
   styleUrl: './qr-scanner.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -37,54 +35,33 @@ export class QrScannerComponent implements AfterViewInit {
   private modalsService = inject(ModalsService);
   private snackService = inject(SnackService)
   private supplyIdTegex = new RegExp("^IdSupply=([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})$");
-  @ViewChild(NgxScannerQrcodeComponent) public scan!: NgxScannerQrcodeComponent;
 
 
   ngAfterViewInit() {
-    this.scan.start();
-    this.scan.devices
-      .pipe(
-        filter(devices => devices.length > 0)
-      )
-      .subscribe(devices => {
-        const device = devices.find(f => (/back|rear|environment/gi.test(f.label)));
-        const idCamera = device ? device.deviceId : devices[0].deviceId;
-        this.camera.setValue(idCamera);
-        this.scan.playDevice(idCamera);
-      });
-    this.camera.valueChanges.subscribe(_ => this.cameraHasChange());
-  }
-  private cameraHasChange() {
-    this.scan.playDevice(this.camera.value!);
-  }
 
-  public async qrDetected(data: ScannerQRCodeResult[]): Promise<void> {
-    if (data.length >= 0)
-      this.scan.stop();
-
-    const match = data[0].value.match(this.supplyIdTegex);
-    if (!match) {
-      this.snackService.showAutoCloseMessage(json.qrInvalid);
-      await sleep(environment.qrDefaultDelay);
-      this.scan.start();
-      return
-    }
-    const idSupply = match[1] ?? '';
-    if (idSupply == '') return;
-
-    await this.store.loadSupply(idSupply);
-    if (this.store.supplySelected.id() == '') return;
-    this.modalsService.closeModal();
-    this.modalsService.showLateralModal('movements');
   }
 
 
+  // public async qrDetected(data: ScannerQRCodeResult[]): Promise<void> {
+  //   if (data.length >= 0)
+  //     this.scan.stop();
 
-  public get cameras(): ScannerQRCodeDevice[] {
-    const devices = this.scan?.devices?.value;
-    if (!devices) return [];
-    return devices;
-  }
+  //   const match = data[0].value.match(this.supplyIdTegex);
+  //   if (!match) {
+  //     this.snackService.showAutoCloseMessage(json.qrInvalid);
+  //     await sleep(environment.qrDefaultDelay);
+  //     this.scan.start();
+  //     return
+  //   }
+  //   const idSupply = match[1] ?? '';
+  //   if (idSupply == '') return;
+
+  //   await this.store.loadSupply(idSupply);
+  //   if (this.store.supplySelected.id() == '') return;
+  //   this.modalsService.closeModal();
+  //   this.modalsService.showLateralModal('movements');
+  // }
+
 
   private get camera(): AbstractControl {
     return this.fbCamera.get('camera')!;
--- src/app/shared/components/qr-reader/qr-reader.component.ts
+++ src/app/shared/components/qr-reader/qr-reader.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ZXingScannerComponent, ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-qr-reader',
   standalone: true,
-  imports: [],
+  imports: [ZXingScannerModule],
   templateUrl: './qr-reader.component.html',
   styleUrl: './qr-reader.component.scss'
 })
 export class QrReaderComponent {
-  public async startCamera(): Promise<void> {
+  public async startCamera(): Promise<HTMLVideoElement> {
     const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
     const video = document.getElementById('video') as HTMLVideoElement;
     video.srcObject = stream;
     video.play();
+    return video;
   }
+  @ViewChild(ZXingScannerComponent) scanner!: ZXingScannerComponent;
+  valueScanned(value: string): void {
+    console.log(value);
+    this.scanner.scanStop();
+  }
+
+  // private async decodeQRCode(video: HTMLVideoElement) {
+  //   const codeReader = new BrowserQRCodeReader();
+  //   while (true) {
+  //     try {
+  //       const result = await codeReader.decodeFromVideoElement(video, undefined);
+  //       if (result) {
+  //         console.log('Código QR detectado:', result.getText());
+  //         // Aquí puedes hacer lo que quieras con el código QR, como enviarlo a tu servidor
+  //       }
+  //     } catch (error) {
+  //       console.error('Error al decodificar el código QR:', error);
+  //     }
+  //     // Esperar un tiempo antes de intentar decodificar de nuevo
+  //     await new Promise(resolve => setTimeout(resolve, 100));
+  //   }
+  // }
+
 }
