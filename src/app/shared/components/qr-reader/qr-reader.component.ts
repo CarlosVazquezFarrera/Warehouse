ccd481b521496bd461ca6767495768de736667fb
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2024-06-14 14:42:49 -0600

    Selecting first camera

--- src/app/shared/components/qr-reader/qr-reader.component.ts
+++ src/app/shared/components/qr-reader/qr-reader.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, inject, input, output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild, inject, input, output } from '@angular/core';
 import QrScanner from 'qr-scanner';
 import { ModalsService } from '@services/modals.service';
 import { MessageService } from '@services/message.service';
@@ -16,13 +16,14 @@ export type QrResult = {
 })
 export class QrReaderComponent implements AfterViewInit, OnDestroy {
 
-
   //#region Properties
   private beep = new Audio("/assets/audio/beep.mp3");
-  private video!: HTMLVideoElement;
   private modalService = inject(ModalsService);
   private messageService = inject(MessageService);
   private qrScanner: QrScanner | undefined;
+
+  @ViewChild('video', { static: false }) video!: ElementRef<HTMLVideoElement>;
+
 
   //#endregion
 
@@ -41,92 +42,42 @@ export class QrReaderComponent implements AfterViewInit, OnDestroy {
 
   //#region Methods
   ngAfterViewInit(): void {
-    this.startCamera();
+    this.initScanner();
   }
-
-  private async startCamera(): Promise<void> {
-    this.video = document.getElementById('video') as HTMLVideoElement;
-    try {
-      const hasCamera = await QrScanner.hasCamera();
-      if (!hasCamera) {
-        this.modalService.closeModal();
-        this.messageService.showMessage('No camera found');
-        return
-      }
-
-      const constraints = {
-        video: {
-          facingMode: 'environment'
-        }
-      };
-
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      this.video.srcObject = stream;
-      this.video.play();
-
-      this.qrScanner = new QrScanner(this.video, result => {
-        this.qrScanned(result);
-      }, {
-        highlightScanRegion: true,
-        highlightCodeOutline: true,
-        maxScansPerSecond: 10
-      });
-
-      await this.qrScanner.start();
-
-    } catch (err) {
-      console.error('Error accessing camera:', err);
-    }
-  }
-
 
   ngOnDestroy(): void {
-    this.stopScanner();
+    this.qrScanner?.stop();
   }
 
-  private async qrScanned(result: QrScanner.ScanResult) {
-    const text = result.data;
-    this.beep.play();
-    if (this.patternMatch()) {
-      const regexPatter = new RegExp(this.patternMatch()!);
-      const match = text.match(regexPatter);
-      if (!match) {
-        this.onQrScanned.emit({ valid: false, text });
+
+  setResult(result: QrScanner.ScanResult) {
+
+  }
+
+  async initScanner() {
+    const videoElem = this.video.nativeElement;
+    this.qrScanner = new QrScanner(videoElem, result => this.setResult(result), {
+      highlightScanRegion: true,
+      highlightCodeOutline: true,
+    });
+
+    try {
+      const hasCamera = await QrScanner.hasCamera();
+
+      if (!hasCamera) {
+        this.messageService.showMessage('Camera not found');
         return;
       }
-      const matchResult = match[this.indexPatternMatch()];
-      this.onQrScanned.emit({ valid: true, text: matchResult })
-    }
-    else
-      this.onQrScanned.emit({ valid: true, text });
 
-    this.stopScanner();
+
+      await this.qrScanner.start();
+      const cameras = await QrScanner.listCameras(true);
+      await this.qrScanner?.setCamera(cameras.at(0)?.id!)
+    }
+    catch {
+
+    }
   }
 
-  private stopScanner(): void {
-
-
-    if (this.qrScanner) {
-      this.qrScanner.stop();
-      if (this.video && this.video.srcObject) {
-        const stream = this.video.srcObject as MediaStream;
-        stream.getTracks().forEach(track => track.stop());
-        this.video.srcObject = null;
-      }
-
-      this.qrScanner.destroy();
-      this.qrScanner = undefined;
-    }
-
-    // const cameras = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-    // cameras.getTracks().forEach(t => t.stop());
-
-    // const videoElem = document.getElementById('video') as HTMLVideoElement;
-    // if (videoElem && videoElem.srcObject) {
-    //   const stream = videoElem.srcObject as MediaStream;
-    //   stream.getTracks().forEach(track => track.stop());
-    //   videoElem.srcObject = null;
-    // }
-  }
   //#endregion
 }
