87e661af1e171d4ecc2af7da0980d97efc69c65c
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2025-06-27 17:45:51 -0600

    Fix: Port removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseApiUrl: 'https://airportwarehouse.com.mx:8080/api/',
+  baseApiUrl: 'https://airportwarehouse.com.mx/api/',
   defaultDebounceTime: 1000,
   pagination: {
     defaultPageSize: 10,
